bb3a6cd1ae97cf180dc46d41f6d8f683c41c350b
retrox <976885345@qq.com>
2016-11-27 11:02:09 +0800

    添加fragment的支持

---
+++ app/src/main/java/com/twtstudio/retrox/wepeiyangrd/auth/login/LoginViewModel.java
@@ -0,0 +1,8 @@
+package com.twtstudio.retrox.wepeiyangrd.auth.login;
+
+/**
+ * Created by retrox on 2016/11/27.
+ */
+
+public class LoginViewModel {
+}
---
+++ app/src/main/java/com/twtstudio/retrox/wepeiyangrd/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.twtstudio.retrox.wepeiyangrd.base;
+
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+
+/**
+ * Created by retrox on 2016/11/27.
+ */
+
+public class BaseActivity extends RxAppCompatActivity {
+}
