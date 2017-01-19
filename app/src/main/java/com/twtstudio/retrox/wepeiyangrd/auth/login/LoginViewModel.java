a4f4b0fbc7b1fe17df441344c537f5e6a3428f21
retrox <976885345@qq.com>
2017-01-19 23:17:19 +0800

    登录界面的完成，等待服务器修复

--- app/src/main/java/com/twtstudio/retrox/wepeiyangrd/auth/login/LoginViewModel.java
+++ app/src/main/java/com/twtstudio/retrox/wepeiyangrd/auth/login/LoginViewModel.java
@@ -1,5 +1,6 @@
 package com.twtstudio.retrox.wepeiyangrd.auth.login;
 
+import android.content.Intent;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import com.kelin.mvvmlight.command.ReplyCommand;
 import com.twtstudio.retrox.wepeiyangrd.api.ApiClient;
 import com.twtstudio.retrox.wepeiyangrd.api.ApiResponse;
 import com.twtstudio.retrox.wepeiyangrd.base.BaseActivity;
+import com.twtstudio.retrox.wepeiyangrd.home.HomeActivity;
 import com.twtstudio.retrox.wepeiyangrd.support.HawkUtil;
 import com.twtstudio.retrox.wepeiyangrd.support.PrefUtils;
 
@@ -27,8 +29,8 @@ public class LoginViewModel implements ViewModel {
     private BaseActivity mActivity;
 
     //model
-    public final ObservableField<String> twtuName = new ObservableField<>("用户名");
-    public final ObservableField<String> twtpasswd = new ObservableField<>("密码");
+    public final ObservableField<String> twtuName = new ObservableField<>();
+    public final ObservableField<String> twtpasswd = new ObservableField<>();
     public Token mToken;
 
     public LoginViewModel(BaseActivity activity) {
@@ -43,10 +45,18 @@ public class LoginViewModel implements ViewModel {
     }
 
     //command
-    public ReplyCommand onLoginClickCommand = new ReplyCommand(this::login);
+    public final ReplyCommand onLoginClickCommand = new ReplyCommand(this::login);
 
-    public ReplyCommand onRegisterClickCommand = new ReplyCommand(() -> {
+    public final ReplyCommand onLaterLoginCommand = new ReplyCommand(this::laterLogin);
+
+    public final ReplyCommand onForgetPasswordCommand = new ReplyCommand(() -> {
+        // TODO: 2017/1/19 fogetpassword jump to act
+        Toast.makeText(mActivity, "fogetpassword", Toast.LENGTH_SHORT).show();
+    });
+
+    public final ReplyCommand onRegisterClickCommand = new ReplyCommand(() -> {
         // TODO: 2016/11/27 跳转到注册页面
+        Toast.makeText(mActivity, "jump to register page", Toast.LENGTH_SHORT).show();
     });
 
     private void login() {
@@ -69,19 +79,23 @@ public class LoginViewModel implements ViewModel {
                     HawkUtil.setIsLogin(true);
                     Toast.makeText(mActivity, "登陆成功", Toast.LENGTH_SHORT).show();
                     // TODO: 2016/11/27 jump to home page
+                    Intent intent = new Intent(mActivity, HomeActivity.class);
+                    mActivity.startActivity(intent);
                 });
 
-        wpyToken.filter(Notification::isOnError)
-                .map(Notification::getValue)
-                .map(ApiResponse::getError_code)
-                .subscribe(integer -> {
-                    switch (integer) {
-                        case 400:
-                            // TODO: 2016/12/17 something
-                            break;
-                    }
-                });
+//        wpyToken.filter(Notification::isOnError)
+//                .map(Notification::getValue)
+//                .map(ApiResponse::getError_code)
+//                .subscribe(integer -> {
+//                    switch (integer) {
+//                        case 400:
+//                            // TODO: 2016/12/17 something
+//                            break;
+//                    }
+//                });
     }
 
-
+    private void laterLogin(){
+        Toast.makeText(mActivity, "laterloginClicked", Toast.LENGTH_SHORT).show();
+    }
 }
